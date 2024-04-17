20ce996ba584c28107f11de9600584e9f9376176
gamsh655 <shittuabdquadri0@gmail.com>
2024-04-17 16:46:30 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "../src/components/Header"
+import Result from "../src/components/Result"
+
+
+function App() {
+  return (
+    <>
+     <Header />
+     <Result/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import logo from "../assets/sail_logo.jpg"
+import "../components/Header.css"
+import { FaMicrophone } from "react-icons/fa6";
+
+const Header = () => {
+  return (
+    <div>
+         <div className="header">
+             <img src={logo} alt="" className='logoIMG' />
+             <h3 className='logoName'>sail search engines</h3>
+         </div>
+          <div className="searchBody">
+            <div className="firstBox">
+        
+           <FaMicrophone className='microPhoneIcone'/>
+          
+           <input type="search" className='input' />
+            </div>
+           
+          </div>
+    </div>
+  )
+}
+
+export default Header
