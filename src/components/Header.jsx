4652f217f75e10fd4cb5d21732933dd54562eeb5
ABDULQUADRI <shittuabdquadri0@gmail.com>
2024-04-18 07:39:47 -0700

    weee' git push

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,19 +2,33 @@ import React from 'react'
 import logo from "../assets/sail_logo.jpg"
 import "../components/Header.css"
 import { FaMicrophone } from "react-icons/fa6";
-
+import { BsSearch } from "react-icons/bs";
+import { FaSun } from 'react-icons/fa6';
+import { FaMoon} from 'react-icons/fa6';
+import { useState } from 'react';
 const Header = () => {
+  const [changeBg , setchangeBg] = useState(false)
+
+  function handleBg(){
+    setchangeBg(!changeBg)
+  }
   return (
-    <div>
-         <div className="header">
+    <div className={changeBg ? "Bg" : "newBg"}>
+         <div className="header" >
              <img src={logo} alt="" className='logoIMG' />
+
              <h3 className='logoName'>sail search engines</h3>
+              <button className="chaneBg" onClick={handleBg}>
+                 {
+                  changeBg ?  <FaMoon  className={changeBg ? "moonIcon" : ""} /> :   <FaSun  className={changeBg ? "sunIcon" : ""}  />
+                 }
+              </button>
          </div>
           <div className="searchBody">
             <div className="firstBox">
                 <div className='innerSpan'>
-                <button className='microPhoneIcone'> <FaMicrophone/></button>
-                 <input type="search" className='input' />
+                <button className='microPhoneIcone'> <BsSearch /></button>
+                 <input type="search" className="input"/>
                 </div>
             </div>
            
